33ef76d5a8eb984511c8fba6faf30c16dd8ec9c4
jfcorugedo <jfcorugedo@gmail.com>
2016-04-11 11:52:16 +0200

    Create main page and test service worker availability

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,7 @@
 'use strict';
 
-// TODO
+if('serviceWorker' in navigator) {
+    console.log('Service worker is supported')
+} else {
+    console.log('This browser does\'t support service workers');
+}
