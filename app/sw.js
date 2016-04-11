831dd5ccdefc5eb6d8fb525c5dc64cda002f64ce
jfcorugedo <jfcorugedo@gmail.com>
2016-04-11 19:05:37 +0200

    handles notification clicks

--- app/sw.js
+++ app/sw.js
@@ -21,6 +21,36 @@ self.addEventListener('push', function(event) {
         self.registration.showNotification(title, {
             body: 'Hey! You have a new notification',
             icon: 'images/icon.png',
+            //Force a different tag so more than one notification can be showed
             tag: 'simple-notification' + Math.random()
         }));
+});
+
+self.addEventListener('notificationclick', function (event) {
+    console.log('notification click. Tag: ', event.notification.tag);
+    //This line is necessary only for android applications, that doesn't close the notification once the user clicks
+    event.notification.close();
+
+    var url = 'https://youtu.be/BKorP55Aqvg?list=RDMhllo1xQer8';
+
+    event.waitUntil(
+        // Gets a list of service worker clients whose origin are the same of this and returns them in a Promise
+        clients.matchAll({
+            type: 'window'
+        })
+        .then(function (windowClients) {
+            console.log('windowClients', windowClients);
+            //If there's a service worker client pointing at the url, reuse it
+            for(var i = 0 ; i < windowClients.length ; i++) {
+                var client = windowClients[i];
+                if(client.url === url && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+
+            if(clients.openWindow) {
+                return clients.openWindow(url);
+            }
+        })
+    );
 });
