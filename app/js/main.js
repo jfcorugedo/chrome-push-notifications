63fdd686938c8cf45e8ad83d3fa9bc8ebc9470d1
jfcorugedo <jfcorugedo@gmail.com>
2016-04-11 12:37:43 +0200

    Firs implementation of service worker lifecycle

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,12 @@
 'use strict';
 
 if('serviceWorker' in navigator) {
-    console.log('Service worker is supported')
+    console.log('Service worker is supported');
+    navigator.serviceWorker.register('sw.js').then(function(reg){
+        console.log('Service worker loaded \uD83D\uDE0E', reg);
+    }).catch(function(err) {
+        console.log('\uD83D\uDE1E', err)
+    });
 } else {
-    console.log('This browser does\'t support service workers');
+    console.log('This browser does\'t support service workers, I\'m afraid \uD83D\uDE23');
 }
